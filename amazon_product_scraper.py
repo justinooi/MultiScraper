f3a4e3c8a912cbd2d63954bfee2ceaf037c0a4b8
Justin Ooi <justinooi@hotmail.sg>
2021-03-09 14:24:21 +0800

    Added Asynchronous Scraping Function & CSV storage.

--- amazon_product_scraper.py
+++ amazon_product_scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import csv
 from requests_html import AsyncHTMLSession
 from abstract_product_scraper import abstract_product_scraper
 from bs4 import BeautifulSoup
@@ -8,22 +9,101 @@ class amazon_product_scraper(abstract_product_scraper):
 
     def __init__(self, url_file):
         self.file = url_file
-        self.product_links = []
+        self.links = []
         self.results = []
+        self.attributes = []
 
     def get_product_links(self):
         with open(self.file) as file_reader:
-            links = file_reader.read().splitlines()
-            self.product_links = ["https://amazon.com/dp/" + s for s in links]
+            self.links = file_reader.read().splitlines()
 
     def asynchronousProcessing(self):
-        pass
+        NUMBER_OF_LAMBDA_FUNCTIONS = 5
+        asession = AsyncHTMLSession()
+        print("Commencing asynchronous requests")
+        print("Number of requesting processes set to 5")
+
+        for i in range(0, len(self.links), NUMBER_OF_LAMBDA_FUNCTIONS):
+            self.results.append(asession.run(  # Calls 5 requests (PROCESSES) in 1 go
+                lambda: self.get_product_details(self.links[i], asession),
+                lambda: self.get_product_details(self.links[i + 1], asession),
+                lambda: self.get_product_details(self.links[i + 2], asession),
+                lambda: self.get_product_details(self.links[i + 3], asession),
+                lambda: self.get_product_details(self.links[i + 4], asession),
+            ))
+        for i in range(len(self.results)):
+            for j in range(len(self.results[0])):
+                self.attributes.append(self.results[i][j])
 
     async def get_product_details(self, link, asession):
-        pass
 
+        items = [None] * 6
+        items[0] = link
+        url = "https://amazon.com/dp/" + link
+
+        webpage = await asession.get(url, headers=self.HEADERS)
+        await webpage.html.arender(sleep=1, timeout=50)
+        soup = BeautifulSoup(webpage.content, "lxml")
+
+        # Get Product Title
+        try:
+            title = soup.find("span", attrs={"id": 'productTitle'})
+            items[1] = title.string.strip().replace(',', '')
+        except AttributeError:
+            items[1] = 'N/A'
+
+        # Get Product Price
+        try:
+            items[2] = soup.find("span", attrs={'id': 'priceblock_ourprice'}).string.strip().replace(',', '')
+        # we are omitting unnecessary spaces
+        # and commas form our string
+        except AttributeError:
+            items[2] = 'N/A'
+
+        # Get Product Rating
+
+        try:
+            items[3] = soup.find("i", attrs={
+                'class': 'a-icon a-icon-star a-star-4-5'}).string.strip().replace(',', '')
+
+        except AttributeError:
+
+            try:
+                items[3] = soup.find(
+                    "span", attrs={'class': 'a-icon-alt'}).string.strip().replace(',', '')
+            except:
+                items[3] = "NA"
+
+        # Get Product Review Count
+
+        try:
+            items[4] = soup.find(
+                "span", attrs={'id': 'acrCustomerReviewText'}).string.strip().replace(',', '')
+
+        except AttributeError:
+            items[4] = "NA"
+
+        # Get Product Availability
+
+        try:
+            available = soup.find("div", attrs={'id': 'availability'})
+            items[5] = available.find("span").string.strip().replace(',', '')
+
+        except AttributeError:
+            items[5] = "NA"
+
+        return tuple(items)
+
+    def store_product_details(self):
+        with open('output-amazon.csv', 'w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerows(self.attributes)
 
 if __name__ == '__main__':
+    time_start = time.time()
     aps = amazon_product_scraper('test.txt')
     aps.get_product_links()
     aps.asynchronousProcessing()
+    aps.store_product_details()
+    time_end = time.time() - time_start
+    print(time_end)
