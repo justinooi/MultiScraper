fabd57c91e268ed9a3e0994b5e26ee2f6b8d1340
Justin Ooi <justinooi@hotmail.sg>
2021-03-10 19:13:54 +0800

    Rotated Browser Header

--- abstract_link_scraper.py
+++ abstract_link_scraper.py
@@ -5,7 +5,7 @@ class abstract_link_scraper(ABC):
 
     searchParameters = ""
     pages = 0
-    HEADERS = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36','Accept-Language': 'en-US, en;q=0.5'})
+    HEADERS = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.2403.157 Safari/537.36','Accept-Language': 'en-US, en;q=0.5'})
     links_list = []
 
     def userInput(self):
