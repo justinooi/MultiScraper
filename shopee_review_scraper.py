6037df30540b5e65da699e58ad75e4d10ed486b5
Justin Ooi <justinooi@hotmail.sg>
2021-03-10 14:34:48 +0800

    Misc. Changes

--- shopee_review_scraper.py
+++ shopee_review_scraper.py
@@ -1,7 +1,6 @@
 import csv
 from requests_html import AsyncHTMLSession
 
-
 class shopee_review_scraper:
     HEADERS = ({'User-Agent':
                     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 "
