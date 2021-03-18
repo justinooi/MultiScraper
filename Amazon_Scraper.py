dd1ddc230e6b5d161f5da178b1c7da79979cd1fb
Eliciacmy <chanminyi97@gmail.com>
2021-03-18 20:15:20 +0800

    Update Amazon_Scraper.py
    docs for amazon_scraper

--- Amazon_Scraper.py
+++ Amazon_Scraper.py
@@ -3,12 +3,12 @@ from amazon_review_scraper import amazon_review_scraper
 from amazon_product_scraper import amazon_product_scraper
 
 class Amazon_Scraper(amazon_product_scraper, amazon_link_scraper, amazon_review_scraper):
-    """Summary
+    """This class inherits all abstract classes and methods for the functions of GUI 
 
     Args:
-        amazon_product_scraper (class): [description]
-        amazon_link_scraper (class): [description]
-        amazon_review_scraper (class): [description]
+        amazon_product_scraper (subclass): child class which allows access to all methods in the class
+        amazon_link_scraper (subclass): child class which allows access to all methods in the class
+        amazon_review_scraper (subclass): child class which allows access to all methods in the class
     """
 
     def __init__(self):
@@ -16,31 +16,31 @@ class Amazon_Scraper(amazon_product_scraper, amazon_link_scraper, amazon_review_
 
     def linkScrape(self, searchParameter, itemQuantity):
         """This function is called upon clicking the submit button, managing the calling of various functions in the base class, 
-           userInput(): to pass in of searchParameter and itemQuantity for encapsulation
+           userInput(): to pass in searchParameter and itemQuantity for encapsulation
            linkSearch(): to retrieve the unique identifier of products 
 
         Args:
             searchParameter (str): name of product user searched for
-            itemQuantity int: number of items
+            itemQuantity (int): number of items
 
         Returns:
-            str: calls the function storeUrl() 
+            function -> str: calls the function storeUrl() 
         """
         super().userInput(searchParameter, itemQuantity)
         super().linkSearch()
         return super().storeUrl()
 
     def productScrape(self, url_file):
-        """This function is called upon clicking the submit button, managing the calling of various functions in other classes, 
+        """This function is called upon clicking the submit button, managing the calling of various functions in other sub classes, 
            amazon_product_scraper class:
-                get_product_links(): 
-                asynchronousProcessing():                                
+                get_product_links(): to get and store the links of the products
+                asynchronousProcessing(): to wait for a specified number of tasks to complete before continuing                             
 
         Args:
-            url_file (str): [description]
+            url_file (str): text file that was returned from linkScrape()
 
         Returns:
-            [type]: [description]
+            function : calls the function store_product_details() 
         """
         amazon_product_scraper.__init__(self, url_file)
         amazon_product_scraper.get_product_links(self)
@@ -48,6 +48,11 @@ class Amazon_Scraper(amazon_product_scraper, amazon_link_scraper, amazon_review_
         return amazon_product_scraper.store_product_details(self)
 
     def reviewScrape(self, url):
+        """This function is called upon clicking the submit button, managing the calling of various functions in other classes,
+
+        Args:
+            url ([type]): [description]
+        """
         amazon_review_scraper.__init__(self, url)
         amazon_review_scraper.asynchronousProcessing(self)
         amazon_review_scraper.store_product_reviews(self)
